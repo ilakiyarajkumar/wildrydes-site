3ed5e5704b5f3b47a7eb42621770f5f91dd4bd35
ilakiyarajkumar <84005810+ilakiyarajkumar@users.noreply.github.com>
2025-01-27 13:33:39 +0530

    Update config.js

--- js/config.js
+++ js/config.js
@@ -3,6 +3,7 @@ window._config = {
         userPoolId: 'ap-south-1_P2MZn72Wm', // e.g. us-east-2_uXboG5pAb
         userPoolClientId: '7ci48g8rbk60bp8081f3v6gjcn', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
         region: 'ap-south-1' // e.g. us-east-2
+        client secret: 'jh9hpn58s8gt5i0488uh11mqu2fjmm303grbbfrorpb0snjlr7e',
     },
     api: {
         invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
