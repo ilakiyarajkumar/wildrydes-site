96cb98f377bddf3838d43b3049404cc9eba8964f
ilakiyarajkumar <84005810+ilakiyarajkumar@users.noreply.github.com>
2025-01-27 13:53:25 +0530

    Update config.js

--- js/config.js
+++ js/config.js
@@ -3,6 +3,7 @@ window._config = {
         userPoolId: 'ap-south-1_P2MZn72Wm', // e.g. us-east-2_uXboG5pAb
         userPoolClientId: '7ci48g8rbk60bp8081f3v6gjcn', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
         region: 'ap-south-1' // e.g. us-east-2
+        
 
     },
     api: {
